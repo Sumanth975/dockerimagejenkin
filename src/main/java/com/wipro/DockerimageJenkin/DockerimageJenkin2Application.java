6cbe0a3c73b222211eb18899d1e38db5c04213ef
Sumanth975 <mbvsumanth@gmail.com>
2022-07-21 07:26:58 +0530

    files commited

---
+++ src/main/java/com/wipro/DockerimageJenkin/DockerimageJenkin2Application.java
@@ -0,0 +1,13 @@
+package com.wipro.DockerimageJenkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerimageJenkin2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerimageJenkin2Application.class, args);
+	}
+
+}
